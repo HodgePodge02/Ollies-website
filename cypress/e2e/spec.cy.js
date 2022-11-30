fbb193ac683b67e2978a9d07394c842f593d5392
HodgePodge02 <false4956@gmail.com>
2022-11-30 18:59:31 -0500

    commitho

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,5 @@
 describe("empty spec", () => {
   it("passes", () => {
-    cy.visit("http://127.0.0.1:5501/index.html");
-  });
-});
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+    cy.visit("https://joliverhodges.com/");
   });
 });
