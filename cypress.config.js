f2731eac2ef67b18057f63cbdf5545d29daee4d1
HodgePodge02 <false4956@gmail.com>
2022-12-12 11:10:19 -0500

    Changing config for url origin issues and DCT

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 3000,
+    experimentalSessionAndOrigin: true,
     baseUrl: "https://joliverhodges.com/",
   },
 });
