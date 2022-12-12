19fadd6ea70fcb649a5cf8d6dbcd95cb4b0aea13
HodgePodge02 <false4956@gmail.com>
2022-12-12 11:45:04 -0500

    pathing issues resolved... sort of

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,4 +1,12 @@
 /// <reference types="cypress"/>
+it("asserts HTML elements are present/visible", () => {
+  cy.visit("/");
+  cy.get("h1").should("be.visible");
+  cy.get("h3").should("be.visible");
+  cy.get("h4").should("be.visible");
+  cy.get("#bigsquare").should("be.visible");
+});
+
 it("use requests to navigation bar links", () => {
   const pages = ["HOME", "CONTACT", "PROJECTS"];
 
@@ -16,7 +24,7 @@ it("use requests to navigation bar links", () => {
     cy.get(cy.get("#contact_Form").should("be.visible"));
     cy.get('[type="name"]').should("be.visible").type("John Doe");
     cy.get('[type="email"]').should("be.visible").type("john-doe@example.com");
-    cy.get("textarea").should("be.visible");
+    cy.get("textarea").should("be.visible").type("I'm John.");
   });
 
   //testing project links
@@ -24,6 +32,11 @@ it("use requests to navigation bar links", () => {
     cy.visit("https://joliverhodges.com/projects/");
     cy.get("#QG_img > a > img").should("be.visible");
     cy.get(":nth-child(1) > .ColumnContent > :nth-child(3) > button").should(
+      "be.visible"
+    );
+    cy.visit("https://joliverhodges.com/projects/");
+    cy.get(":nth-child(2) > img").should("be.visible");
+    cy.get(":nth-child(2) > .ColumnContent > :nth-child(3) > button").should(
       "be.visible"
     );
   });
