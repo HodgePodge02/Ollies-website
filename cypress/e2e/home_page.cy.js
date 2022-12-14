f98e50adb5fae8b8cdb63c2573a2ff1123c6a8ac
HodgePodge02 <false4956@gmail.com>
2022-12-14 13:08:01 -0500

    Final touch ups

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -7,7 +7,7 @@ it("asserts HTML elements are present/visible", () => {
   cy.get("#bigsquare").should("be.visible");
 });
 
-it("use requests to navigation bar links", () => {
+it("Tests nav bar links", () => {
   const pages = ["HOME", "CONTACT", "PROJECTS"];
 
   cy.visit("/");
@@ -17,27 +17,26 @@ it("use requests to navigation bar links", () => {
       cy.request(link.prop("href"));
     });
   });
+});
+//CONTACT FORM
+it("Fills out the contact form", () => {
+  cy.visit("https://joliverhodges.com/contact/");
+  cy.get(cy.get("#contact_Form").should("be.visible"));
+  cy.get('[type="name"]').should("be.visible").type("John Doe");
+  cy.get('[type="email"]').should("be.visible").type("john-doe@example.com");
+  cy.get("textarea").should("be.visible").type("I'm John.");
+});
 
-  //CONTACT FORM
-  describe("Fills out the contact form", () => {
-    cy.visit("https://joliverhodges.com/contact/");
-    cy.get(cy.get("#contact_Form").should("be.visible"));
-    cy.get('[type="name"]').should("be.visible").type("John Doe");
-    cy.get('[type="email"]').should("be.visible").type("john-doe@example.com");
-    cy.get("textarea").should("be.visible").type("I'm John.");
-  });
-
-  //testing project links
-  context("Testing the live links/img links of the PROJECTS page", () => {
-    cy.visit("https://joliverhodges.com/projects/");
-    cy.get("#QG_img > a > img").should("be.visible");
-    cy.get(":nth-child(1) > .ColumnContent > :nth-child(3) > button").should(
-      "be.visible"
-    );
-    cy.visit("https://joliverhodges.com/projects/");
-    cy.get(":nth-child(2) > img").should("be.visible");
-    cy.get(":nth-child(2) > .ColumnContent > :nth-child(3) > button").should(
-      "be.visible"
-    );
-  });
+//testing project links
+it("Testing the live links/img links of the PROJECTS page", () => {
+  cy.visit("https://joliverhodges.com/projects/");
+  cy.get("#QG_img > a > img").should("be.visible");
+  cy.get(":nth-child(1) > .ColumnContent > :nth-child(3) > button").should(
+    "be.visible"
+  );
+  cy.visit("https://joliverhodges.com/projects/");
+  cy.get(":nth-child(2) > img").should("be.visible");
+  cy.get(":nth-child(2) > .ColumnContent > :nth-child(3) > button").should(
+    "be.visible"
+  );
 });
